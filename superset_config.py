6a457d1832e4c31c5280654ac118670ffdf90fa1
Laura <laura.c@uptimeanalytics.com>
2020-02-10 10:24:40 -0500

    :bug: typo

--- superset_config.py
+++ superset_config.py
@@ -20,9 +20,9 @@ SECRET_KEY = '\2\1thisismyscretkey\1\2\e\y\y\h'
 # Note that the connection information to connect to the datasources
 # you want to explore are managed directly in the web UI
 SQLALCHEMY_DATABASE_URI = 'postgresql://{}:{}@timescaledb/{}'.format(
-    os.get_env('POSTGRES_USER'),
-    os.get_env('POSTGRES_PASSWORD'),
-    os.get_env('POSTGRES_DB'),
+    os.getenv('POSTGRES_USER'),
+    os.getenv('POSTGRES_PASSWORD'),
+    os.getenv('POSTGRES_DB'),
 )
 
 # Flask-WTF flag for CSRF
